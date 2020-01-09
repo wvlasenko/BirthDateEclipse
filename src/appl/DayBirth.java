38e0db64b110f3cfa380d05391ba624e775e6703
Wolodymyr <wvlasenko@gmail.com>
2020-01-09 01:44:36 -0800

    Add commentto code

--- src/appl/DayBirth.java
+++ src/appl/DayBirth.java
@@ -11,16 +11,18 @@ public class DayBirth {
 
 	public static void main(String[] args) {
 		LocalDate currDate = LocalDate.now();
+		// check current data
 		LocalDate myBirthDate = LocalDate.of(1981, 04, 30);
+		// get user data birth
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 		String txt = myBirthDate.format(formatter);
 		System.out.println("My birth date=" + txt);
 		Long amount = myBirthDate.until(currDate, ChronoUnit.DAYS);
-		System.out.println("I have " + amount + "-"+"day old");
+		System.out.println("I have " + amount + "-" + "day old");
 		amount = myBirthDate.until(currDate, ChronoUnit.MONTHS);
-		System.out.println("I have " + amount +"-"+"monts old");
+		System.out.println("I have " + amount + "-" + "monts old");
 		amount = myBirthDate.until(currDate, ChronoUnit.YEARS);
-		System.out.println("I have " + amount + "-"+"years old");
+		System.out.println("I have " + amount + "-" + "years old");
 		DayOfWeek dof = myBirthDate.getDayOfWeek();
 		Locale locale = new Locale("en");
 		System.out.println("I was born on " + dof.getDisplayName(TextStyle.FULL, locale));
